b5f3f9b16e7dba92bb211e99e53ae301be1a837b
XWxiaowei <2809641033@qq.com>
2018-06-07 07:33:12 +0800

    JDK 动态代理

---
+++ tiny-spring-copy/src/main/java/com/jay/spring/aop/JdkDynamicAopProxy.java
@@ -0,0 +1,9 @@
+package com.jay.spring.aop;
+
+/**
+ * Created by xiang.wei on 2018/6/7
+ *
+ * @author xiang.wei
+ */
+public class JdkDynamicAopProxy {
+}
---
+++ tiny-spring-copy/src/main/java/com/jay/spring/aop/ReflectiveMethodInvocation.java
@@ -0,0 +1,50 @@
+package com.jay.spring.aop;
+
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Method;
+
+/**
+ * Joinpoint 被拦截到的点，
+ * 因为Spring 只支持方法类型的连接点，所以在Spring中连接点指的就是被拦截到的方法，实际上连接点还可以是字段或构造器
+ * Created by xiang.wei on 2018/6/7
+ *
+ * @author xiang.wei
+ */
+public class ReflectiveMethodInvocation implements MethodInvocation {
+    private Object target;
+    private Method method;
+    private Object[] args;
+
+    public ReflectiveMethodInvocation(Object target, Method method, Object[] args) {
+        this.target = target;
+        this.method = method;
+        this.args = args;
+    }
+
+    @Override
+    public Method getMethod() {
+        return method;
+    }
+
+    @Override
+    public Object[] getArguments() {
+        return args;
+    }
+
+    @Override
+    public Object proceed() throws Throwable {
+        return method.invoke(target, args);
+    }
+
+    @Override
+    public Object getThis() {
+        return target;
+    }
+
+    @Override
+    public AccessibleObject getStaticPart() {
+        return method;
+    }
+}
