5c1488b625aa6218d27d876353ab5ccd9174e6a3
XWxiaowei <2809641033@qq.com>
2018-06-25 07:06:31 +0800

    第三周代码实现，项目结构调整

---
+++ liu-spring-demo/src/test/java/com/jay/spring/test/v2/ApplicationContextTestV2.java
@@ -0,0 +1,30 @@
+package com.jay.spring.test.v2;
+
+import com.jay.spring.context.ApplicationContext;
+import com.jay.spring.context.support.ClassPathXmlApplicationContext;
+import com.jay.spring.context.support.FileSystemXmlApplicationContext;
+import com.jay.spring.service.v1.PetStoreService;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by xiang.wei on 2018/6/18
+ *
+ * @author xiang.wei
+ */
+public class ApplicationContextTestV2 {
+    @Test
+    public void testGetBeans() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("petstore-v1.xml");
+        PetStoreService petStoreService = (PetStoreService) applicationContext.getBean("petStoreService");
+        Assert.assertNotNull(petStoreService);
+    }
+    @Test
+    public void testGetBeanFileSystemContext() {
+//        TODO  1.全路径写死了
+
+        ApplicationContext ctx = new FileSystemXmlApplicationContext("/Volumes/Develop/spring-source/spring-learn/liu-spring-demo/src/test/resources/petstore-v1.xml");
+        PetStoreService petStoreService = (PetStoreService) ctx.getBean("petStoreService");
+        Assert.assertNotNull(petStoreService);
+    }
+}
