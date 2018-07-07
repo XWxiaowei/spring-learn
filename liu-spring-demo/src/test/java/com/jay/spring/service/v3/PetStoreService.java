3a5e48ec40d802f83c3859455018538ff1dae96e
XWxiaowei <2809641033@qq.com>
2018-07-07 12:33:02 +0800

    单元测试代码修改

---
+++ liu-spring-demo/src/test/java/com/jay/spring/service/v3/PetStoreService.java
@@ -0,0 +1,34 @@
+package com.jay.spring.service.v3;
+
+import com.jay.spring.dao.v2.AccountDao;
+import com.jay.spring.dao.v2.ItemDao;
+
+/**
+ * @author xiang.wei
+ * @create 2018/6/11 14:42
+ */
+public class PetStoreService {
+    private AccountDao accountDao;
+    private ItemDao itemDao;
+    private int version;
+
+    public PetStoreService(AccountDao accountDao, ItemDao itemDao, int version) {
+        this.accountDao = accountDao;
+        this.itemDao = itemDao;
+        this.version = version;
+    }
+
+
+    public AccountDao getAccountDao() {
+        return accountDao;
+    }
+
+    public ItemDao getItemDao() {
+        return itemDao;
+    }
+
+
+    public int getVersion() {
+        return version;
+    }
+}
--- liu-spring-demo/src/test/java/com/jay/spring/test/v2/ApplicationContextTestV2.java
+++ liu-spring-demo/src/test/java/com/jay/spring/test/v2/ApplicationContextTestV2.java
@@ -2,9 +2,14 @@ package com.jay.spring.test.v2;
 
 import com.jay.spring.context.ApplicationContext;
 import com.jay.spring.context.support.ClassPathXmlApplicationContext;
+import com.jay.spring.dao.v2.AccountDao;
+import com.jay.spring.dao.v2.ItemDao;
 import com.jay.spring.service.v2.PetStoreService;
 import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 
 /**
  * Created by xiang.wei on 2018/6/18
@@ -18,6 +23,13 @@ public class ApplicationContextTestV2 {
         PetStoreService petStoreService = (PetStoreService) applicationContext.getBean("petStoreService");
         Assert.assertNotNull(petStoreService.getAccountDao());
         Assert.assertNotNull(petStoreService.getItemDao());
+
+        assertTrue(petStoreService.getAccountDao() instanceof AccountDao);
+        assertTrue(petStoreService.getItemDao() instanceof ItemDao);
+
+        assertEquals("xiangwei",petStoreService.getOwner());
+        assertEquals(2, petStoreService.getVersion());
+
     }
 
 }
